5034c47861b732b6133b52556d2e12f9fa5db984
J33569 <jblanchet@leseminaire.ca>
2026-01-14 08:27:22 -0500

    fix

--- mainscript.js
+++ mainscript.js
@@ -1,6 +1,10 @@
 function Lightdarktoggle() {
-    const bodyElements = document.body.querySelectorAll("*");
-    bodyElements.forEach(el => {
-        el.classList.toggle("dark-mode");
-    });
+   const bodyElements = document.body.querySelectorAll("*");
+   bodyElements.forEach(el => {
+      el.classList.toggle("dark-mode");
+   });
+}
+function ldtb() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
 }
