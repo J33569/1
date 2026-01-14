96157413315a9f8fe744d5ba876869262cbcf292
J33569 <jblanchet@leseminaire.ca>
2026-01-13 20:55:44 -0500

    ,.

--- mainscript.js
+++ mainscript.js
@@ -1,6 +1,6 @@
 function Lightdarktoggle() {
-   var allElements = document.querySelectorAll("*");
-   allElements.forEach(function (el) {
-      el.classList.toggle("dark-mode");
-   });
+    const allElements = document.querySelectorAll("*");
+    allElements.forEach(el => {
+        el.classList.toggle("dark-mode");
+    });
 }
